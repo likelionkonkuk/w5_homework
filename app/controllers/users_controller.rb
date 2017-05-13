563ccbcd6ef49e3e95380d5c9a7aa295b025abdf
Shinmyungsang <mongddangand@naver.com>
2017-05-13 23:21:43 +0000

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+   before_action :find_user, only: [:show, :edit, :update, :destroy]
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find (params[:id])
+  end
+  
+  def new  
+    @user = User.new
+  end
+ 
+  def create
+  	@user = User.new(user_params)
+
+  	if @user.save
+  		redirect_to @user
+  	else
+  		render :new
+  	end  	
+  end
+ 
+  def edit
+  end
+  
+  def update
+   if @user.update(user_params)
+  	  redirect_to @user  
+   else
+  	  render :new
+  	end
+  end
+  	
+  def destroy
+    @user.destroy
+    redirect_to root_path
+  end
+   private
+
+  def user_params  	
+  	params.require(:user).permit(
+  	:name,
+  	:phone,
+  	:email,
+  	:address
+  	)
+  end
+  
+  private 
+
+  def find_user
+  	@user = User.find(params[:id])
+  end
+end
